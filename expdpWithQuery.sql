48fdca441ded7aebb174d2455536f295d9672215
Baki Sahin <bakisahin@yahoo.com>
2021-06-02 02:36:02 +0300

    Update expdpWithQuery.sql

--- expdpWithQuery.sql
+++ expdpWithQuery.sql
@@ -12,3 +12,14 @@ query='CUSTOMER.ALZ_HLTH_MDLR_LOG:"WHERE ((application_name = ''CREATE_PROPOSAL'
              or application_name = ''KOC_HLTH_WEB_INT_REN_ROUTE''
              or application_name = ''BEFORE_POL_RESTORE''
              or application_name = ''POL_RESTORE'')"'
+
+
+
+set numformat 99999999999999999999999
+col dp format a100
+col nt format a5
+select 'query=''DCS_YKB_LIVE.S_DCMNT:"WHERE ID between '||min(id)||' and '||max(id)||'"''' dp, count(*), nt
+ from ( select id, ntile(5) over (order by id) nt
+ from DCS_YKB_LIVE.S_DCMNT
+ )
+ group by nt order by 3;
